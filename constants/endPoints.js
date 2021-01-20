6836efb3439db80fc360d86c93572d97e6d7ecc0
Trainee 18 <sagar.vartak@contentstack.com>
2021-01-20 18:14:32 +0530

    api end points

---
+++ constants/endPoints.js
@@ -0,0 +1,5 @@
+const url =
+  "https://raw.githubusercontent.com/sagar-vartak/DataServer/main/data/";
+export const contentstackURI = url + "contentstack.json";
+export const rawengURI = url + "rawengg.json";
+export const surfboardURI = url + "surfboard.json";
